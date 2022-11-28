c063f689974cba4b24033aa108bd2a45ceb06269
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2022-11-28 14:21:23 +0100

    28/22/2022
    Tilføjet unittest projekt og gjort klar til test

---
+++ Eksamen-ClassLibrary/LibraryUnitTest/SensorTest.cs
@@ -0,0 +1,33 @@
+﻿using Eksamen_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryUnitTest
+{
+    [TestClass]
+    public class SensorTest
+    {
+        Sensor sensor = new Sensor { };
+
+        [TestMethod]
+        public void ValidateMacAddress()
+        {
+
+        }
+
+        [TestMethod]
+        public void ValidateName()
+        {
+
+        }
+
+        [TestMethod]
+        public void ValidateTest()
+        {
+
+        }
+    }
+}
