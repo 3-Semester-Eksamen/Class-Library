7fec121ea6bc84dd79e03400001f6d06c2d2de63
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2022-11-30 13:34:41 +0100

    KeyDone

--- Eksamen-ClassLibrary/Eksamen-ClassLibrary/Key.cs
+++ Eksamen-ClassLibrary/Eksamen-ClassLibrary/Key.cs
@@ -15,17 +15,20 @@ namespace Eksamen_ClassLibrary
 
         public void ValidateName()
         {
-
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentNullException("Navnet må ikke være tomt");
+            if (Name.Length < 3) throw new ArgumentOutOfRangeException("Navnet skal være længere end 1 karakter");
         }
 
         public void ValidateEmail()
         {
-
+            if (string.IsNullOrWhiteSpace(Email)) throw new ArgumentNullException("Email må ikke være tomt");
+            if (Email.Length < 3) throw new ArgumentOutOfRangeException("Email skal være længere end 1 karakter");
         }
 
         public void ValidatePhone()
         {
-
+            if (string.IsNullOrWhiteSpace(Phone)) throw new ArgumentNullException("Telefon må ikke være tomt");
+            if (Phone.Length < 3) throw new ArgumentOutOfRangeException("Telefon skal være længere end 1 karakter");
         }
 
         public void Validate()
--- Eksamen-ClassLibrary/LibraryUnitTest/KeyTest.cs
+++ Eksamen-ClassLibrary/LibraryUnitTest/KeyTest.cs
@@ -5,15 +5,20 @@ namespace LibraryUnitTest
     [TestClass]
     public class KeyTest
     {
-        Key keyNameNull = new Key { Id = 1, Name = null, Email = "Key1@gmail.com", Phone = "12345678" };
-        Key keyNameTooShort = new Key { Id = 1, Name = "K", Email = "Key1@gmail.com", Phone = "12345678" };
-        Key keyNameTooLong = new Key { Id = 1, Name = "KeyNavnErAltForLangtDetGårSletIkkeDetHer", Email = };
+        Key keyCorrect = new Key { Id = 1, Name = "Key1", Email = "Key1@gmail.com", Phone = "12345678" };
+        Key keyNameNull = new Key { Id = 2, Name = null, Email = "Key1@gmail.com", Phone = "12345678" };
+        Key keyNameTooShort = new Key { Id = 3, Name = "Ke", Email = "Key1@gmail.com", Phone = "12345678" };
+        Key keyEmailNull = new Key { Id = 4, Name = "KeyErGod", Email = null, Phone = "123456" };
+        Key keyEmailTooShort = new Key { Id = 5, Name = "KeyErGod", Email = "Em", Phone = "123456" };
+        Key keyPhoneNull = new Key { Id = 6, Name = "KeyErGod", Email = "Email", Phone = null };
+        Key keyPhoneTooShort = new Key { Id = 7, Name = "KeyErGod", Email = "Email", Phone = "12" };
+
 
 
         [TestMethod]
         public void ValidateNameTest()
         {
-            keyNameNull.ValidateName();
+            keyCorrect.ValidateName();
             Assert.ThrowsException<ArgumentNullException>(() => keyNameNull.ValidateName());
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyNameTooShort.ValidateName());
 
@@ -22,19 +27,29 @@ namespace LibraryUnitTest
         [TestMethod]
         public void ValidateEmail()
         {
+            Assert.ThrowsException<ArgumentNullException>(() => keyEmailNull.ValidateEmail());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyEmailTooShort.ValidateEmail());
 
         }
 
         [TestMethod]
         public void ValidatePhone()
         {
+            Assert.ThrowsException<ArgumentNullException>(() => keyPhoneNull.ValidatePhone());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyPhoneTooShort.ValidatePhone());
 
         }
 
         [TestMethod]
         public void ValidateTest()
         {
-
+            keyCorrect.Validate();
+            Assert.ThrowsException<ArgumentNullException>(() => keyNameNull.ValidateName());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyNameTooShort.ValidateName());
+            Assert.ThrowsException<ArgumentNullException>(() => keyEmailNull.ValidateEmail());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyEmailTooShort.ValidateEmail());
+            Assert.ThrowsException<ArgumentNullException>(() => keyPhoneNull.ValidatePhone());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => keyPhoneTooShort.ValidatePhone());
         }
     }
 }
