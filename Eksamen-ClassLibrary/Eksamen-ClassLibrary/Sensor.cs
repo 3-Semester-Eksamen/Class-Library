d40b58b78e7a721eba78a261d588594a1a59347c
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2022-11-30 09:42:52 +0100

    Update Sensor.cs

--- Eksamen-ClassLibrary/Eksamen-ClassLibrary/Sensor.cs
+++ Eksamen-ClassLibrary/Eksamen-ClassLibrary/Sensor.cs
@@ -1,7 +1,29 @@
 ﻿namespace Eksamen_ClassLibrary
 {
-    public class Class1
+    public class Sensor
     {
+        public string MacAddress { get; set; }
+        public string Name { get; set; }
 
+        public void ValidateMacAddress()
+        {
+
+        }
+
+        public void ValidateName()
+        {
+
+        }
+
+        public void Validate()
+        {
+            ValidateMacAddress();
+            ValidateName();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(MacAddress)}={MacAddress}, {nameof(Name)}={Name}}}";
+        }
     }
 }
